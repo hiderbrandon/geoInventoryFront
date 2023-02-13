b56bcb2f876427141c3f3ec52dd2da8e345e3366
hider brandon vargas <hiderbrandon@gmail.com>
2023-02-12 21:48:02 -0500

    finally plot a marker sucessfully

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,24 +1,39 @@
-import {Map, Marker, GeolocateControl } from "react-map-gl";
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl
+} from 'react-map-gl';
+import { useMemo, useState } from "react";
+
 import {config} from "../../config"
 import "./map.css"
-import { useState } from "react";
-
+import CITIES from './cities.json';
 const apiKey = config.mapboxToken;
 
 function MyMap() {
 
-  const [markers, setMarkers] = useState([
-    {
-      longitude: -122.431297,
-      latitude: 37.7749,
-    },
-    {
-      longitude: -74.005974,
-      latitude: 40.712776,
-    }
-  ]);
-  
+  const [popupInfo, setPopupInfo] = useState(null);
 
+  const pins = useMemo(()=> CITIES.map((city, index) => (
+    <Marker
+      key={`marker-${index}`}
+      longitude={city.longitude}
+      latitude={city.latitude}
+      anchor="bottom"
+      onClick={e => {
+        // If we let the click event propagates to the map, it will immediately close the popup
+        // with `closeOnClick: true`
+        e.originalEvent.stopPropagation();
+        setPopupInfo(city);
+      }}
+    >
+      <div className='marker'></div>
+    </Marker>
+    )
+  ));
 
   return (
     <div className="map-container">
@@ -28,16 +43,16 @@ function MyMap() {
           longitude: -76.534293,
           latitude: 3.372799,
           zoom: 15,
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-      >
-        <Marker longitude={-76.534293} latitude={3.372799} anchor="top" >
-          <img src="./markerIcon.png" width="80" height="80" 
-            onLoad={() => console.log("Image Loaded")}
-            onError={() => console.log("Error Loading Image")}
-          />
-        </Marker>
+        }} 
+        mapStyle='mapbox://styles/mapbox/light-v11'
+        >
+          
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
 
+        {pins}
+      
       </Map>
     </div>
   );
